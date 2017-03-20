7098b34548a045bc3e8d4a3549922e2e3fb2f5e2
sundy <sundyli@truxing.com>
2017-03-20 14:16:27 +0800

    update robot demo

--- examples/robot-demo.go
+++ examples/robot-demo.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
-	"net/http"
 	"time"
 
 	robot "github.com/go-vgo/robotgo"
@@ -22,71 +21,62 @@ var (
 	LinkPoint  = Point{1638, 608}
 	ClosePoint = Point{1251, 163}
 	InputPoint = Point{1372, 768}
-	client     *http.Client
 
-	stop bool
+	stop        bool
+	sleepSecond int
 )
 
 func initFlag() {
+	flag.IntVar(&sleepSecond, "s", 5, "sleep second")
 	flag.Parse()
 }
 
 func initPoint() {
-	fmt.Println("输入点击链接的位置")
-	sleep(2)
-	x, y := robot.GetMousePos()
-	LinkPoint = Point{x, y}
-	fmt.Println(LinkPoint)
-
-	//fmt.Println("输入点击关闭微信文章窗口的位置")
-	//sleep(2)
-	//x, y = robot.GetMousePos()
-	//ClosePoint = Point{x, y}
-	//fmt.Println(ClosePoint)
-
-	fmt.Println("输入输入框的位置")
-	sleep(2)
-	x, y = robot.GetMousePos()
-	InputPoint = Point{x, y}
-	fmt.Println(InputPoint)
-	fmt.Println("点击输入框")
-	fmt.Println("点击F8退出")
-	sleep(2)
-}
-
-// n second
-func sleep(n int) {
-	time.Sleep(time.Duration(n) * time.Second)
-}
-
-func NextUrl() string {
-	return _urls[rand.Intn(len(_urls))]
+	fmt.Println("鼠标移动到点击链接的位置(f3结束)")
+	key := robot.AddEvent("f3")
+	sleep(1)
+	if key == 0 {
+		x, y := robot.GetMousePos()
+		LinkPoint = Point{x, y}
+		fmt.Println("链接地址", x, y)
+	}
+	fmt.Println("鼠标移动到输入框的位置(f3结束)")
+	key = robot.AddEvent("f3")
+	sleep(1)
+	if key == 0 {
+		x, y := robot.GetMousePos()
+		InputPoint = Point{x, y}
+		fmt.Println("链接地址", x, y)
+	}
+	fmt.Println("程序开始,点击F8退出")
 }
 
 func stopRobot() {
-	key := robot.AddEvent("f8")
-	if key == 0 {
-		log.Println("stop")
-		stop = true
+	for {
+		key := robot.AddEvent("f8")
+		sleep(1)
+		if key == 0 {
+			stop = !stop
+			if stop {
+				log.Println("stopped")
+			} else {
+				log.Println("started")
+			}
+		}
 	}
 }
 
 func process() {
 	fmt.Printf("requested: %d %s \n", counter, time.Now().String())
-	u := NextUrl()
-	if u == "" {
-		sleep(10)
+	nextUrl := NextUrl()
+	if nextUrl == "" {
 		return
 	}
-	robot.TypeString(u)
+	robot.TypeString(nextUrl)
 	robot.KeyTap("enter")
 	robot.MoveMouseSmooth(LinkPoint.x, LinkPoint.y, 0.5)
 	robot.Click(LinkPoint.x, LinkPoint.y)
 
-	//
-	//robot.MoveMouseSmooth(ClosePoint.x, ClosePoint.y, 0.5)
-	//robot.Click(ClosePoint.x, ClosePoint.y)
-	//
 	robot.MoveMouseSmooth(InputPoint.x, InputPoint.y, 0.5)
 	robot.Click(InputPoint.x, InputPoint.y)
 
@@ -97,16 +87,26 @@ func init() {
 	initFlag()
 	initPoint()
 }
-func start() {
+
+func main() {
 	go stopRobot()
-	for !stop {
+	for {
+		if stop {
+			time.Sleep(1 * time.Second)
+			continue
+		}
 		process()
-		time.Sleep(time.Second * 5)
+		sleep(sleepSecond)
 	}
 }
 
-func main() {
-	start()
+// n second
+func sleep(n int) {
+	time.Sleep(time.Duration(n) * time.Second)
+}
+
+func NextUrl() string {
+	return _urls[rand.Intn(len(_urls))]
 }
 
 var (
